2ea8c90f749e7dc3b1cd1ccc40baf17c4792a69c
Lancelot <Lancelot@Alcyone-VM>
2022-07-24 18:53:11 -0400

    Nevermind, I found the bug haha
    I had accidentally copied <script> tags...

--- User/Frontend/components/result-screen.js
+++ User/Frontend/components/result-screen.js
@@ -1,3 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                linen: "#ffede1",
+                ivory: "#f9fbf2",
+                'light-cyan': "#d7f9ff",
+                'baby-blue-eyes': "#afcbff",
+                'oxford-blue': "#0e1c36",
+            },
+            transitionProperty: {
+                'timer-width': 'width'
+            }
+        }
+    }
+}
 document.addEventListener('alpine:init', () => {
     Alpine.store('results', {
         sentences: [],
